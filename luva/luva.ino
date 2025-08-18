301d2c6eee467d2abd9d6d06d1eeb1255ce14048
agath410v3 <agatha.lovelace.flor@gmail.com>
2025-08-18 14:58:19 -0300

    atualizado código

--- luva/luva.ino
+++ luva/luva.ino
@@ -152,7 +152,6 @@ void loop() {
   }  
   checkSingleFinger();
   controlFingerLEDs();
-  //terminal code: rosrun rosserial_python serial_node.py _port:=/dev/ttyUSB0 _baud:=57600
   nh.spinOnce();
   
 }
